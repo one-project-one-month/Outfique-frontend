7aea86c144703dc948800e86d8835d39d14d1790
Terry Thant <doomvicii@gmail.com>
2025-09-27 01:45:39 +0630

    feat:nativewind and rnreusable setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-background">
+      <View className="flex-1 justify-center items-center p-6">
+        <View className="items-center">
+          <Text variant="h1" className="mb-2 text-8xl text-center">
+            Welcome to Outfique
+          </Text>
+          <Text variant="lead" className="text-center mb-8 text-2xl">
+            Your daily outfit inspiration.
+          </Text>
+        </View>
+        <Button className="bg-black px-5 py-3 rounded-lg" size="lg" onPress={() => alert("Let's go!")}>
+          <Text className="text-white text-xl font-semibold">Get Started</Text>
+        </Button>
+      </View>
+    </SafeAreaView>
   );
 }
